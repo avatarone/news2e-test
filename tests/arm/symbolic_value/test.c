51b625eeb032d2ae5f25550a478c66d7b47092eb
Jonas Zaddach <zaddach@eurecom.fr>
2016-08-02 09:00:12 +0000

    Added more verbose output to symbolic value test

--- tests/arm/symbolic_value/test.c
+++ tests/arm/symbolic_value/test.c
@@ -13,10 +13,13 @@ void main(void)
 {
     int x = 0;
     s2e_make_symbolic(&x, sizeof(x), "x");
+    s2e_print_expression(x, "x before constraints");
     if (x == 0) {
+        s2e_print_expression(x, "x in if branch");
         s2e_kill_state(0, "If branch terminated");
     }
     else {
+        s2e_print_expression(x, "x in else branch");
         s2e_kill_state(0, "Else branch terminated");
     }
 }
